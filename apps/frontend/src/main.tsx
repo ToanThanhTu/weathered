2330eac449793ecfb6dfd1434669656961c90c48
Trevor <toantu3005@gmail.com>
2026-04-13 15:04:04 +1000

    feat: add error boundary

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,14 +1,17 @@
+import { ErrorBoundary } from '@/components/ErrorBoundary'
+import { queryClient } from '@/lib/query-client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from '@/lib/query-client'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
